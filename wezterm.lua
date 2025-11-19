50aaeb875c81fe24a6d1892b925d21f107820eb5
ownssec <ownssec@gmail.com>
2025-11-20 04:22:59 +0800

    update wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -1,11 +1,10 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
--- Use JetBrainsMono Nerd Font with fallbacks
 config.font = wezterm.font_with_fallback({
-	{ family = "JetBrainsMono Nerd Font", weight = "Regular", style = "Normal" },
-	"Symbols Nerd Font Mono", -- extra glyph coverage
-	"Noto Color Emoji", -- emoji support
+  "JetBrainsMono Nerd Font Mono",
+  "Symbols Nerd Font Mono",
+  "Noto Color Emoji",
 })
 
 config.font_size = 10.5
@@ -19,7 +18,8 @@ config.color_scheme = "Catch Me If You Can (terminal.sexy)"
 config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 
--- Disable missing glyph warnings
 config.warn_about_missing_glyphs = false
 
 return config
+
+
