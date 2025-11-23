a8e1237d8a9b053d5ef1f74d9318ef05e961b136
ownssec <ownssec@gmail.com>
2025-11-24 00:30:48 +0800

    added: line height and cell width for wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -2,9 +2,9 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font_with_fallback({
-  "JetBrainsMono Nerd Font Mono",
-  "Symbols Nerd Font Mono",
-  "Noto Color Emoji",
+	"JetBrainsMono Nerd Font Mono",
+	"Symbols Nerd Font Mono",
+	"Noto Color Emoji",
 })
 
 config.font_size = 10.5
@@ -20,6 +20,9 @@ config.hide_tab_bar_if_only_one_tab = true
 
 config.warn_about_missing_glyphs = false
 
+config.freetype_load_flags = "NO_HINTING"
+config.freetype_load_target = "Light"
+config.line_height = 1
+config.cell_width = 1
+
 return config
-
-
